1b85f78c95e7e2cccbb8b0e1b1de1a62e5eb3e8c
Pascual Soto Uribe <pascualsu@gmail.com>
2021-02-26 17:46:07 -0300

    creción directorio para guardar los drafts de la Ficha Académica junto al primer draft de información de personas

---
+++ ficha-academica/informacion-personas/draftInformacionPersonas.sql
@@ -0,0 +1,97 @@
+WITH
+    CuboPersonas
+    AS
+    (
+        SELECT top 30
+            CP.COD_PERS           AS cod_pers
+        , CP.RUT                AS rut
+        , NBP.check_digit       AS dv
+        , NBP.first_name        AS nombre
+        , LEFT(APELLIDO_PATERNO, 1) + LOWER(SUBSTRING(APELLIDO_PATERNO, 2, LEN(APELLIDO_PATERNO)))  AS apellido_paterno 
+        , LEFT(APELLIDO_MATERNO, 1) + LOWER(SUBSTRING(APELLIDO_MATERNO, 2, LEN(APELLIDO_MATERNO)))  AS apellido_materno
+        , NBP.sex               AS sexo
+        , CP.FECHA_NACIM        AS fecha_nacimiento
+        , NBP.country_of_origin AS pais
+        FROM CORP.PERS                      AS CP
+            LEFT JOIN NORMALIZADO_BANNER.PERSON AS NBP
+            ON CP.RUT = NBP.rut
+
+    )
+
+SELECT top 10
+    *
+from GENERAL.GOREMAL
+
+
+SELECT top 10
+    apellido_paterno
+from CORP.PERS
+
+select *
+from information_schema.columns
+where table_name = 'PERS' and table_schema = 'CORP'
+select top 10
+    *
+from corp.pers
+/*
+COD_PERS
+COD_INSTITUCION_SALUD
+NOM_PERS
+APELLIDO_MATERNO
+APELLIDO_PATERNO
+COD_PARAM_77_VIG
+NOM_PERS_ABREV
+OTRA_DIRECCION_ELECTRON
+RUT
+COD_PARAM_8_SEXO
+COD_PARAM_23_ESTADO_CIVIL
+DIG_VERIF
+FECHA_NACIM
+DIRECCION_INTERNET
+DIRECCION_PUC
+COD_PAIS_ORIGEN
+COD_PAIS_NACIONALIDAD
+COD_PARAM_230_TIPO_SALUD
+NOM_OTRO_TIPO_SALUD
+NOM_SEGURO_COMPLEMENTARIO
+RUT_COTIZANTE
+NOM_SOCIAL
+*/
+
+select *
+from information_schema.columns
+where table_name = 'PERSON' and table_schema = 'MDIUC'
+select top 10
+    *
+from MDIUC.PERSON
+/*
+rut
+dv
+tipo_id
+paternal_last_name
+maternal_last_name
+name
+created_at
+*/
+
+select *
+from information_schema.columns
+where table_name = 'PERSON' and table_schema = 'NORMALIZADO_BANNER'
+select top 10
+    *
+from NORMALIZADO_BANNER.PERSON
+/*
+rut
+check_digit
+first_name
+paternal_last_name
+maternal_last_name
+sex
+birthdate
+dead_at
+banner_person_id
+nationality
+country_of_origin
+is_alive
+*/
+
--- mallaMDM/pruebaMallaMDM.sql
+++ mallaMDM/pruebaMallaMDM.sql
@@ -2,7 +2,43 @@ select * from information_schema.tables where table_schema like '%mdiuc%'
 
 -- Tables prepared for testing purposes
 SELECT * FROM MDIUC.PERSON_TEST
+SELECT *
+FROM MDIUC.PERSON_TEST_2
 SELECT * FROM MDIUC.COUNTRY
+
+select *
+from MCP_MDIUC.PROCESSED_PERSON_TEST
+where rut in ('14545147', '15337074')
+select *
+from MCP_MDIUC.PROCESSING_PERSON_TEST
+where primary_key in ('14545147', '15337074')
+
+select *
+from MCP_MDIUC.CONFLICTIVE_PERSON_TEST
+--primero en la tabla y luego a databricks; la corrección se carga luego desde databricks hacia RESOLUTION
+select *
+from MCP_MDIUC.RESOLUTION_PERSON_TEST
+
+exec sp_help 'MCP_MDIUC.PROCESSED_PERSON_TEST'
+
+update MCP_MDIUC.ENTITY 
+set is_valid = 
+    case 
+        when is_valid = 1 then 0
+        else 1 
+    end;
+
+-- TRUNCATE TABLE MCP_MDIUC.PROCESSED_PERSON_TEST
+-- TRUNCATE TABLE MCP_MDIUC.PROCESSING_PERSON_TEST
+
+TRUNCATE TABLE MDIUC.PERSON_TEST
+
+EXEC UTILS.ColumnTypes N'MDIUC', N'PERSON_TEST'
+-- EN EL PASO 0 LA TABLA SÍ SE CREA MANUAL
+
+/* RESOLUCIÓN DE CONFLICTOS */
+
+-- Si los grupos existen en CONFLICTIVE, se resuelven en RESOLUTION
 
 -- Tables we have to manually populate
 SELECT * FROM MCP_MDIUC.ENTITY
@@ -12,6 +48,10 @@ SELECT * FROM MCP_MDIUC.ENTITY_ATTRIBUTE_SYSTEM_MAPPING
 SELECT * FROM MCP_MDIUC.GROUPING_RULE
 SELECT * FROM MCP_MDIUC.COMPARING_RULE
 
+DELETE MCP_MDIUC.ENTITY_ATTRIBUTE WHERE entity_attribute_id in (73, 77)
+UPDATE MCP_MDIUC.ENTITY_ATTRIBUTE_SYSTEM_MAPPING SET attribute_rank = 2 WHERE entity_system_table_id = 33
+UPDATE MCP_MDIUC.COMPARING_RULE SET parameter = '' where rule_type = N'exact'
+
 -- UPDATE MCP_MDIUC.ENTITY SET is_valid = 0 WHERE entity_id = 19
 
 -- We can safely truncate theses ones
@@ -20,9 +60,12 @@ TRUNCATE TABLE MCP_MDIUC.GROUPING_RULE
 TRUNCATE TABLE MCP_MDIUC.ENTITY_ATTRIBUTE_SYSTEM_MAPPING
 
 -- But, due to some of their columns being referenced as FK by other tables, we cannot truncate these tables. So we delete their rows
-DELETE FROM MCP_MDIUC.ENTITY_SYSTEM_TABLE WHERE primary_key IN (SELECT entity_system_table_id FROM MCP_MDIUC.ENTITY_SYSTEM_TABLE)
-DELETE FROM MCP_MDIUC.ENTITY_ATTRIBUTE WHERE is_primary_key IN (SELECT entity_attribute_id FROM MCP_MDIUC.ENTITY_ATTRIBUTE)
-DELETE FROM MCP_MDIUC.ENTITY WHERE is_valid IN (SELECT entity_id FROM MCP_MDIUC.ENTITY)
+DELETE FROM MCP_MDIUC.ENTITY_SYSTEM_TABLE WHERE entity_system_table_id IN (SELECT entity_system_table_id
+FROM MCP_MDIUC.ENTITY_SYSTEM_TABLE)
+DELETE FROM MCP_MDIUC.ENTITY_ATTRIBUTE WHERE entity_attribute_id IN (SELECT entity_attribute_id
+FROM MCP_MDIUC.ENTITY_ATTRIBUTE)
+DELETE FROM MCP_MDIUC.ENTITY WHERE entity_id IN (SELECT entity_id
+FROM MCP_MDIUC.ENTITY)
 
 -- Once this is done, we procede to populate the tables
 -- ENTITY
@@ -46,6 +89,17 @@ VALUES (18, N'rut', 'nvarchar', 1)
     ,  (19, 'is_active', 'int', 0)
     ,  (19, 'end_date', 'datetime2', 0)
 
+declare @entityId int;
+set @entityId = (select entity_id
+from mcp_mdiuc.entity
+where name = 'PERSON_TEST')
+SELECT @entityId
+
+select *
+from UTILS.ColumnDescrition ('comuna', 'cod_comuna')
+select *
+from [UTILS].[ColumnType] ('mdiuc', 'person')
+
 -- ENTITY_SYSTEM_TABLE
 INSERT INTO MCP_MDIUC.ENTITY_SYSTEM_TABLE(entity_id, [schema_name], table_name, primary_key)
 VALUES (18, 'NORMALIZADO_TEST', 'PERSON_BANNER', 'rut')
@@ -60,11 +114,15 @@ VALUES (80, 34, 'NOM_PAIS_INGLES', 1)
 
 -- GROUPING_RULE
 INSERT INTO MCP_MDIUC.GROUPING_RULE
-VALUES (71)
+VALUES
+(74),
+(75)
 
 -- COMPARING_RULE
 INSERT INTO MCP_MDIUC.COMPARING_RULE (entity_attribute_id, rule_type, parameter, process)
-VALUES (71, 'exact', NULL, 'concat')
+VALUES
+(74, 'fuzzy', 70, 'concat'),
+(75, 'fuzzy', 70, 'concat')
 
 
 
@@ -81,4 +139,9 @@ SELECT TOP 3 * FROM UC_BANNER.PAIS
 SELECT TOP 3 * FROM NORMALIZADO_PEOPLE_SOFT.COUNTRY
 
 
-SELECT TOP 50 * FROM MCP_MDIUC.LOG ORDER BY Log_id DESC
+SELECT TOP 14
+    *
+FROM MCP_MDIUC.LOG
+ORDER BY log_id DESC
+
+exec sp_help 'MCP_MDIUC.PROCESSING_PERSON_TEST'
